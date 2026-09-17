8c4a029cb31e75e3eddd361f238a9003711e0cf0
Pratick-saha <waterhasiratomioka07@gmail.com>
2026-09-17 23:27:31 +0530

    updated state by pratick

---
+++ state/state.py
@@ -0,0 +1,14 @@
+class state:
+    def creat_account():
+        print("acc created")
+
+    def get_balance():
+        print("get balance")
+
+    def set_balance():
+        print("set balance!!")
+
+st=state()
+st.creat_account()
+st.get_balance()
+st.set_balance()
